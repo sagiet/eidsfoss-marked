4a766f98b041d627d8295a8ae65ac2bfe4d99b17
Eidsfoss Marked <eidsfossmarked@gmail.com>
2025-02-13 14:03:38 +0100

    Lagt til config.js og oppdatert index.html

---
+++ config.js
@@ -0,0 +1,6 @@
+// Google Drive API konfigurasjon
+const localConfig = {
+    CLIENT_ID: '416088722781-fotidqc1oalagfns19s53atr80ifoorr.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyAR6lryU4b_rzVh7bgX2sWW4yj5LKMZpSQ',
+    FOLDER_ID: '1DwJRY9rMD5x2bIZCWmPZrxvlFE1jRoXY'
+};
