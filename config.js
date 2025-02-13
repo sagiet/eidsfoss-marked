ce6945dfddd323bbc9aac63accdbb93da17fa573
Eidsfoss Marked <eidsfossmarked@gmail.com>
2025-02-13 14:47:28 +0100

    Oppdatert mappe-ID for Google Drive

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Drive API konfigurasjon
 window.config = {
     API_KEY: 'AIzaSyAR6lryU4b_rzVh7bgX2sWW4yj5LKMZpSQ',
-    FOLDER_ID: '1vllR5LEaGOILcVApMfM_ygq2pcv3S75f'
+    FOLDER_ID: '1q69DfRrRHDZQ5UxjCc4myYZluTM7e4Kf'
 };
